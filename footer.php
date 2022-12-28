2cb062240748ea2350b3b8aec3d33eb1f967605e
Dorian <80327420+dorianml@users.noreply.github.com>
2022-12-28 22:10:56 +0100

    first commit on Main - add header and Footer in PHP

---
+++ footer.php
@@ -0,0 +1,15 @@
+</main>
+<?php get_sidebar(); ?>
+</div>
+<footer id="footer" role="contentinfo">
+    <div id="footerImage">
+        <?php echo ' <img id="footer" src=" '. get_template_directory_uri() . "-child" . '/images/footer.png" '?> ;
+    </div>
+<div id="mentionsLegales">
+<?php echo ' <a href="http://www.planty.com/mentionslegales">Mentions Légales </a>'  ; ?>
+</div>
+</footer>
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> <?php blankslate_schema_type(); ?>>
+
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>" />
+    <meta name="viewport" content="width=device-width" />
+    <?php wp_head(); ?>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Syne:wght@400;500;600;700;800&display=swap" rel="stylesheet">
+</head>
+
+<body <?php body_class(); ?>>
+    <?php wp_body_open(); ?>
+    <div id="wrapper" class="hfeed">
+        <header id="header" role="banner">
+            <div id="branding">
+                <div id="site-title" itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
+                    <?php
+                    if (is_front_page() || is_home() || is_front_page() && is_home()) {
+                        echo '<a href="' . esc_url(home_url('/')) . '">
+                        <img id="logo" src=" ' . get_template_directory_uri() . "-child" . '/images/logo.png">
+                        </a>';
+                    }
+                    ?>
+                </div>
+                <div id="site-description" <?php if (!is_single()) {
+                                                echo ' itemprop="description"' . ' "DESCRIPTION ';
+                                            } ?>><?php bloginfo('description'); ?></div>
+            </div>
+            <nav id="menu" class="menu" role="navigation" itemscope itemtype="https://schema.org/SiteNavigationElement">
+                <?php wp_nav_menu(array(['theme_location' => 'menu',
+                                         'container' => false,
+                                          'link_before' => '<span itemprop="name">',
+                                           'link_after' => '</span>',
+                                           'menu_class' => 'navBar-header'
+                                           ])); ?>
+            </nav>
+        </header>
+        <div id="container">
+            <main id="content" role="main">
